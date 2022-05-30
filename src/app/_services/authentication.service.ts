8e3aadc9b616e4b8a218dea1927264152ab83037
PoojaSarda123 <105299282+PoojaSarda123@users.noreply.github.com>
2022-05-30 11:20:14 +0530

    Assignment-Pooja Sarda

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,42 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+import { config } from 'process';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+    private router: Router;
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username: string, password: string) {
+        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+            .pipe(map(users => {
+                // login successful if there's a jwt token in the response
+                if (users && users.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(users));
+                    this.currentUserSubject.next(users);
+                }
+                return users;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
